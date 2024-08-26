f42f12c1fb33e676d44b0e5cb7718504f3c8cb40
Maxou305 <maxime.chazard@le-campus-numerique.fr>
2024-08-26 09:45:08 +0200

    Calculator class added

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public float divide(int a, int b) {
+        return (float) a / b;
+    }
+}
